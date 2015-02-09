d9c5f8ed3864844c81cb01aad40497022119b1c1
adevianin <remba93@gmail.com>
2015-02-10 01:41:46 +0200

    Event, GameEvent, PlayerEvent entities were added

---
+++ src/AppBundle/Entity/GameEvent.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use AppBundle\Validator\Constraints as CustomAssert;
+
+/**
+ * @ORM\Entity
+ */
+class GameEvent extends Event
+{
+    /**
+     * @ORM\ManyToMany(targetEntity="Game")
+     * @CustomAssert\EntitiesExist(associatedEntity="Game", message="games with ids %ids% are non-exist")
+     */
+    private $games;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->games = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add games
+     *
+     * @param  \AppBundle\Entity\Game $games
+     * @return GameEvent
+     */
+    public function addGame(\AppBundle\Entity\Game $games)
+    {
+        $this->games[] = $games;
+
+        return $this;
+    }
+
+    /**
+     * Remove games
+     *
+     * @param \AppBundle\Entity\Game $games
+     */
+    public function removeGame(\AppBundle\Entity\Game $games)
+    {
+        $this->games->removeElement($games);
+    }
+
+    /**
+     * Get games
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getGames()
+    {
+        return $this->games;
+    }
+}
