fcd92b5c9998f028f13e034c344c30402eb8e738
adevianin <remba93@gmail.com>
2015-02-10 04:50:50 +0200

    TeamRole entity was added

---
+++ src/AppBundle/Entity/TeamRole.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @UniqueEntity("name", message="name is already used")
+ */
+class TeamRole
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=30, unique=true, nullable=false)
+     * @Assert\Regex("/^[A-zА-я іїє]{2,30}$/", message="name is not valid")
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param  string   $name
+     * @return TeamRole
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
