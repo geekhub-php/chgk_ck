7a217bea3a7bc9617fb5e159653fe5524fbac26d
adevianin <remba93@gmail.com>
2015-03-16 20:08:35 +0200

    added UserAdmin

---
+++ src/AppBundle/Admin/UserAdmin.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class UserAdmin extends Admin
+{
+    protected $baseRouteName = "admin_users";
+    protected $baseRoutePattern = "users";
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+       		//->add('roles', 'tags')
+            ->add('assignedPlayer', 'entity', array(
+                'class' => 'AppBundle:Player',
+                'property' => 'lastName',
+            ))
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+        	//->add('roles', null, array(), 'tags')
+            ->add('assignedPlayer', null, array(), 'entity', array('property' => 'lastName', 'placeholder' => 'any player'))
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('id')
+			//->add('roles', 'array')
+            ->add('assignedPlayer', null, array('associated_property' => 'lastName'))
+        ;
+    }
+	
+	protected function configureRoutes(RouteCollection $collection)
+	{
+	    $collection->remove('create');
+	}
+}
