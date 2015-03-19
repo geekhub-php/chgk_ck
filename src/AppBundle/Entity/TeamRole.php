73d4921d3a43db1a55351b4a62bc8c261017abcc
slavka-jd <miledi777@ukr.net>
2015-03-20 01:36:34 +0200

    Added necessary constants

--- src/AppBundle/Entity/TeamRole.php
+++ src/AppBundle/Entity/TeamRole.php
@@ -15,6 +15,16 @@ class TeamRole
 {
     use TimestampableTrait;
 
+    const NAME_CAPTAIN = 'Капитан';
+    const NAME_SPARROW = 'Ласточка';
+    const NAME_IDEAGEN = 'Генератор идей';
+    const NAME_LOGICIAN = 'Логик';
+    const NAME_INTUITIONIST = 'Интуит';
+    const NAME_ERUDITE = 'Эрудит';
+    const NAME_CRITICIST = 'Критик';
+    const NAME_LIFE_OF_THE_TEAM = 'Душа команды';
+    const NAME_USSR = 'Совок';
+
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id
@@ -29,6 +39,21 @@ class TeamRole
      */
     private $name;
 
+    public static function getNames()
+    {
+        return [
+            teamRole:: NAME_CAPTAIN => 'Капитан',
+            teamRole:: NAME_SPARROW => 'Ласточка',
+            teamRole:: NAME_IDEAGEN => 'Генератор идей',
+            teamRole:: NAME_LOGICIAN => 'Логик',
+            teamRole:: NAME_INTUITIONIST => 'Интуит',
+            teamRole:: NAME_ERUDITE => 'Эрудит',
+            teamRole:: NAME_CRITICIST => 'Критик',
+            teamRole:: NAME_LIFE_OF_THE_TEAM => 'Душа команды',
+            teamRole:: NAME_USSR => 'Совок',
+        ];
+    }
+
     /**
      * Get id
      *
@@ -40,19 +65,6 @@ class TeamRole
     }
 
     /**
-     * Set name
-     *
-     * @param  string   $name
-     * @return TeamRole
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
      * Get name
      *
      * @return string
@@ -60,5 +72,18 @@ class TeamRole
     public function getName()
     {
         return $this->name;
+    }
+
+    /**
+     * Set name
+     *
+     * @param  string $name
+     * @return TeamRole
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
     }
 }
