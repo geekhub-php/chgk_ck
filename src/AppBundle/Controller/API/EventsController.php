746d6f9680efabdee923a5374df485746f690113
adevianin <remba93@gmail.com>
2015-03-28 20:38:07 +0200

    added filters to events

--- src/AppBundle/Controller/API/EventsController.php
+++ src/AppBundle/Controller/API/EventsController.php
@@ -7,22 +7,43 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use AppBundle\Entity\Event;
 use FOS\RestBundle\Controller\Annotations as RestAnnotations;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\Annotations as REST;
 
 class EventsController extends FOSRestController
 {
 	/**
 	 * @RestAnnotations\View(serializerGroups={"eventFull", "short"})
+	 * @REST\Get("events")
+	 * @REST\QueryParam(name="title", default="")
+	 * @REST\QueryParam(name="authorId", requirements="\d+", default="")
+	 * @REST\QueryParam(name="date", requirements="\d+", default="")
 	 * @ApiDoc(
 	 * 	description="returns events",
 	 * 	statusCodes={
 	 * 		200="ok",
 	 * 	},
+	 * 	filters={
+     *      {"name"="title", "dataType"="string"},
+	 * 		{"name"="authorId", "dataType"="integer"},
+	 * 		{"name"="date", "dataType"="integer"}
+     *  },
 	 * 	output="AppBundle\Entity\Event"
 	 * )
 	 */
-    public function getEventsAction()
+    public function getEventsAction($title, $authorId, $date)
     {
-    	return $this->getDoctrine()->getRepository('AppBundle:Event')->findAll();
+    	$criteria = [];
+		if ($title) {
+			$criteria['title'] = $title;
+		}
+		if ($authorId) {
+			$criteria['author'] = $authorId;
+		}
+		if ($date) {
+			$criteria['eventDate'] = $date;
+		}
+		
+    	return $this->getDoctrine()->getRepository('AppBundle:Event')->findBy($criteria);
     }
 	
 	/**
