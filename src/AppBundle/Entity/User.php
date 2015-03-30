fcd16340f36604cda870ebf696c2999e1ebc474b
jackjeka <angie.or.jackie@gmail.com>
2015-03-30 20:06:08 +0300

    configured custom user provider

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -20,6 +20,12 @@ class User extends BaseUser
      */
     protected $id;
 
+    /** @ORM\Column(name="facebook_id", type="string", length=255, nullable=true) */
+    protected $facebook_id;
+
+    /** @ORM\Column(name="facebook_access_token", type="string", length=255, nullable=true) */
+    protected $facebook_access_token;
+
     /**
      * @ORM\OneToOne(targetEntity="Player")
      */
@@ -85,5 +91,51 @@ class User extends BaseUser
         $this->assignedPlayer = $assignedPlayer;
 
         return $this;
+    }
+
+    /**
+     * Set facebook_id
+     *
+     * @param string $facebookId
+     * @return User
+     */
+    public function setFacebookId($facebookId)
+    {
+        $this->facebook_id = $facebookId;
+
+        return $this;
+    }
+
+    /**
+     * Get facebook_id
+     *
+     * @return string 
+     */
+    public function getFacebookId()
+    {
+        return $this->facebook_id;
+    }
+
+    /**
+     * Set facebook_access_token
+     *
+     * @param string $facebookAccessToken
+     * @return User
+     */
+    public function setFacebookAccessToken($facebookAccessToken)
+    {
+        $this->facebook_access_token = $facebookAccessToken;
+
+        return $this;
+    }
+
+    /**
+     * Get facebook_access_token
+     *
+     * @return string 
+     */
+    public function getFacebookAccessToken()
+    {
+        return $this->facebook_access_token;
     }
 }
