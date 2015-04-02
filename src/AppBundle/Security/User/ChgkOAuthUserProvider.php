a3033ff1dbded8a853df9a63b3a9a85e50a40583
jackjeka <angie.or.jackie@gmail.com>
2015-04-02 08:21:25 +0300

    code cleanup

--- src/AppBundle/Security/User/ChgkOAuthUserProvider.php
+++ src/AppBundle/Security/User/ChgkOAuthUserProvider.php
@@ -2,53 +2,41 @@
 
 namespace AppBundle\Security\User;
 
-//use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUserProvider;
 use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
 use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider as BaseClass;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class ChgkOAuthUserProvider extends BaseClass
-
 {
-
-    /**
-     * {@inheritDoc}
-     */
     public function connect(UserInterface $user, UserResponseInterface $response)
     {
         $property = $this->getProperty($response);
         $username = $response->getUsername();
 
-        //on connect - get the access token and the user ID
         $service = $response->getResourceOwner()->getName();
 
         $setter = 'set'.ucfirst($service);
         $setter_id = $setter.'Id';
         $setter_token = $setter.'AccessToken';
 
-        //we "disconnect" previously connected users
         if (null !== $previousUser = $this->userManager->findUserBy(array($property => $username))) {
             $previousUser->$setter_id(null);
             $previousUser->$setter_token(null);
             $this->userManager->updateUser($previousUser);
         }
 
-        //we connect current user
         $user->$setter_id($username);
         $user->$setter_token($response->getAccessToken());
 
         $this->userManager->updateUser($user);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function loadUserByOAuthUserResponse(UserResponseInterface $response)
     {
         $username = $response->getUsername();
         $email = $response->getEmail();
         $user = $this->userManager->findUserBy(array($this->getProperty($response) => $username));
-        //when the user is registrating
+
         if (null === $user) {
             $service = $response->getResourceOwner()->getName();
             $setter = 'set'.ucfirst($service);
@@ -60,22 +48,19 @@ class ChgkOAuthUserProvider extends BaseClass
             $user->$setter_token($response->getAccessToken());
             $user->setUsername($username);
             $user->setEmail($email);
-//            $user->setPassword($password);
             $user->setEnabled(true);
             $this->userManager->updateUser($user);
+
             return $user;
         }
 
-        //if user exists - go with the HWIOAuth way
         $user = parent::loadUserByOAuthUserResponse($response);
 
         $serviceName = $response->getResourceOwner()->getName();
-        $setter = 'set' . ucfirst($serviceName) . 'AccessToken';
+        $setter = 'set'.ucfirst($serviceName).'AccessToken';
 
-        //update access token
         $user->$setter($response->getAccessToken());
 
         return $user;
     }
-
 }
