06110cb2d6276d3ec79cbba3ba6b1272480e7564
adevianin <remba93@gmail.com>
2015-04-11 11:03:23 +0300

    fixed entity validation to type І,Ї,Є and ' in city name and player's name

--- src/AppBundle/Entity/Team.php
+++ src/AppBundle/Entity/Team.php
@@ -46,7 +46,7 @@ class Team
     /**
      * @ORM\Column(type="string", length=100, nullable=false)
      * @Assert\NotBlank()
-     * @Assert\Regex("/^[A-zА-я іїє\-]{2,255}$/u", message="city is not valid")
+     * @Assert\Regex("/^[A-zА-я ІіЇїЄє'\-]{2,255}$/u", message="city is not valid")
      * @JMS\Groups({"teamFull"})
      */
     private $city;
