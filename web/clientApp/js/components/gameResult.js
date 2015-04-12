3c5c3ee5e7569e37dc283f7c12bed12ed94f0331
adevianin <remba93@gmail.com>
2015-04-12 06:58:23 +0300

    made refreshable opinions on game results

--- web/clientApp/js/components/gameResult.js
+++ web/clientApp/js/components/gameResult.js
@@ -3,7 +3,7 @@ angular.module('gameResult', [])
 	$scope.gameResults = gameResultAPI.getGameResults($routeParams.gameId);
 	$scope.makeResultOpinion = gameResultAPI.makeResultOpinion;
 }])
-.factory('gameResultAPI', ['$resource', 'opinionAPI', '$routeParams', function($resource, opinionAPI, $routeParams){
+.factory('gameResultAPI', ['$resource', 'opinionAPI', '$routeParams', 'opinionableModel', function($resource, opinionAPI, $routeParams, opinionableModel){
 	var gameResultUrl = '/api/games/:gameId/gameResults/:gameResultId';
 	var gameId = $routeParams.gameId;	
 	
@@ -15,7 +15,7 @@ angular.module('gameResult', [])
 					opinionAPI.setParentUrl(gameResultUrl, {gameId: gameId, gameResultId: result.id});
 					result.opinions = opinionAPI.getOpinions();
 					result.opinions.$promise.then(function(){
-						opinionAPI.fillOpinionableStats(result);					
+						opinionableModel.fillOpinionableStats(result);					
 					});		
 				});			
 			});
@@ -23,9 +23,11 @@ angular.module('gameResult', [])
 			return results;		
 		},
 		
-		makeResultOpinion: function(gameResultId, is_positive){
-			opinionAPI.setParentUrl(gameResultUrl, {gameId: gameId, gameResultId: gameResultId});
-			opinionAPI.makeOpinion(is_positive);		
+		makeResultOpinion: function(gameResult, is_positive){
+			opinionAPI.setParentUrl(gameResultUrl, {gameId: gameId, gameResultId: gameResult.id});
+			opinionAPI.makeOpinion(is_positive).then(function(addedOpinion){
+				opinionableModel.addNewOpinion(gameResult, addedOpinion);		
+			});		
 		}	
 	};
 }]);
